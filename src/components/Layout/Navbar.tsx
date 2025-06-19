4f25fa43cbeeb72a666f7786a337db5d775f1e01
jacksonhehe <jackcano14@gmail.com>
2025-06-18 19:22:31 -0500

    Wrap Navbar links in nav and add aria state

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -4,23 +4,31 @@ import styles from './Navbar.module.css'
 
 export default function Navbar() {
   const [open, setOpen] = useState(false)
+  const navId = 'main-nav'
 
   return (
     <header className={styles.navbar}>
       <Link to="/" className={styles.logo}>La Virtual Zone</Link>
-      <button className={styles.hamburger} onClick={() => setOpen(!open)}>
+      <button
+        className={styles.hamburger}
+        aria-expanded={open}
+        aria-controls={navId}
+        onClick={() => setOpen(!open)}
+      >
         <span></span>
         <span></span>
         <span></span>
       </button>
-      <ul className={`${styles.links} ${open ? styles.open : ''}`}>
-        <li><Link to="/" onClick={() => setOpen(false)}>Inicio</Link></li>
-        <li><Link to="/liga-master" onClick={() => setOpen(false)}>Liga Master</Link></li>
-        <li><Link to="/torneos" onClick={() => setOpen(false)}>Torneos</Link></li>
-        <li><Link to="/galeria" onClick={() => setOpen(false)}>Galería</Link></li>
-        <li><Link to="/blog" onClick={() => setOpen(false)}>Blog</Link></li>
-        <li><Link to="/login" onClick={() => setOpen(false)}>Iniciar Sesión</Link></li>
-      </ul>
+      <nav>
+        <ul id={navId} className={`${styles.links} ${open ? styles.open : ''}`}>
+          <li><Link to="/" onClick={() => setOpen(false)}>Inicio</Link></li>
+          <li><Link to="/liga-master" onClick={() => setOpen(false)}>Liga Master</Link></li>
+          <li><Link to="/torneos" onClick={() => setOpen(false)}>Torneos</Link></li>
+          <li><Link to="/galeria" onClick={() => setOpen(false)}>Galería</Link></li>
+          <li><Link to="/blog" onClick={() => setOpen(false)}>Blog</Link></li>
+          <li><Link to="/login" onClick={() => setOpen(false)}>Iniciar Sesión</Link></li>
+        </ul>
+      </nav>
     </header>
   )
 }
