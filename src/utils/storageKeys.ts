2b0afc85b8ce9c12d0eac0f96ffbbe578d5720ab
jacksonhehe <jackcano14@gmail.com>
2025-06-23 17:23:21 -0500

    feat: add calendar page

--- src/utils/storageKeys.ts
+++ src/utils/storageKeys.ts
@@ -1,3 +1,4 @@
 export const VZ_USERS_KEY = 'vz_users';
 export const VZ_CURRENT_USER_KEY = 'vz_current_user';
 export const VZ_COMMENTS_KEY = 'vz_comments';
+export const VZ_CALENDAR_PREFS_KEY = 'vz_calendar_prefs';
