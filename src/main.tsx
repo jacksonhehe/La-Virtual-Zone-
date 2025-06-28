f2bfc438196156cff82ac574040343f35db66838
jacksonhehe <jackcano14@gmail.com>
2025-06-28 17:01:51 -0500

    Add global error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import AppErrorBoundary from './components/AppErrorBoundary';
 import './index.css';
 import seed from './data/seed.json';
 import { VZ_CLUBS_KEY, VZ_PLAYERS_KEY, VZ_FIXTURES_KEY } from './utils/storageKeys';
@@ -20,7 +21,9 @@ if (typeof localStorage !== 'undefined' && !localStorage.getItem('vz_initialized
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AppErrorBoundary>
+        <App />
+      </AppErrorBoundary>
     </BrowserRouter>
   </StrictMode>
 );
