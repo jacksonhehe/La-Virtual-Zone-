8c5b674d20aeea1e5dd5e1dc5c590ff8e52e472d
jacksonhehe <jackcano14@gmail.com>
2025-07-06 07:56:37 -0500

    a

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    supportFile: false,
     specPattern: 'tests/e2e/**/*.cy.{ts,js}',
     baseUrl: 'http://localhost:5173',
   },
